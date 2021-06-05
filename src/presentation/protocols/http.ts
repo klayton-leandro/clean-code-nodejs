3042ca0ec205bf2e74dbfd5d9f1ebe6fb6f0654a
klayton.paula <klayton_leandro54@hotmail.com>
2021-06-04 23:31:08 -0300

    refactor: make SignUpController provider

--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,5 @@
-export interface HttpResponse {
-    statusCode: number
+export interface HttpRequest {
     body: any
 }
 
-export interface HttpRequest {
-    body?: any
-}
+export interface HttpResponse { statusCode: number, body?: any }
