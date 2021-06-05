dcdefb67cb28817f1889b1550e2f7203f0e56aa0
klayton.paula <klayton_leandro54@hotmail.com>
2021-06-04 22:41:34 -0300

    refactor: ensure SignUpController.handle

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,14 @@
+import { HttpRequest } from '../protocols/http'
+
 export class SignUpController {
-    handle(httpRequest: any): any {
-        if(!httpRequest.body.name){
+    handle (httpRequest: HttpRequest): any{
+        if (!httpRequest.body.name){
             return {
                 statusCode: 400,
                 body: new Error('Missing param: name')
             }
         }
-        if(!httpRequest.body.email){
+        if (!httpRequest.body.email){
             return {
                 statusCode: 400,
                 body: new Error('Missing param: email')
