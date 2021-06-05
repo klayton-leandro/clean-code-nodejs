66a2206f34a2884eb9b9e0d9805a426b7d943cdf
klayton.paula <klayton_leandro54@hotmail.com>
2021-06-04 23:18:25 -0300

    test: add change mod passwordConfirmation

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,7 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../erros/missing-param-error'
 
-
-describe('SingUp Controller', () => { 
+describe('SingUp Controller', () => {
     test('Should return 400 if no name is provided', () => {
         const sut = new SignUpController()
         const httpRequest = {
@@ -43,5 +42,19 @@ describe('SingUp Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    })
+
+    test('Should return 400 if no passwordConfirmation is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_email@email.com",
+                password: "any_password"
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password_confirmation'))
     })
 })
