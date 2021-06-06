c92c0ba4fac42aea5cb186841e965a2c328b24be
klayton.paula <klayton_leandro54@hotmail.com>
2021-06-06 01:17:25 -0300

    refactor: move sut create to a factory create helpers method

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,9 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 describe('Account Mongo Repository', () => {
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository()
+    }
     beforeAll(async () => {
         await MongoHelper.connect('mongodb+srv://klayton:leandro11@cluster0.kvtap.mongodb.net/test')
     })
@@ -9,7 +12,7 @@ describe('Account Mongo Repository', () => {
         await MongoHelper.disconnect()
     })
     test('Should Return an account on success ', async () => {
-        const sut = new AccountMongoRepository()
+        const sut = makeSut()
         const account = await sut.add({
             name: 'any_name',
             email: 'any_email@mail.com',
